782e281b4215c736c1a7d686486ac828a2690289
Mahmud Burhanudin <mahmud.bin_burhanudin@kcl.ac.uk>
2023-10-12 19:17:55 +0100

    Extend the form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
 """Forms of the project."""
 
 # Create your forms here.
+
+from django import forms
+
+class ThingForm(forms.Form):
+    """Form for adding things to the database."""
+    name = forms.CharField(max_length=35, required=True)
+    description = forms.Textarea(max_length=120, required=False)
+    quantity = forms.NumberInput(min_value=0, max_value=50, required=True)
+    
