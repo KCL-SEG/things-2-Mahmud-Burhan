ceca98ea4d9de45029f54123136e2ddf7cc149e4
Mahmud Burhanudin <mahmud.bin_burhanudin@kcl.ac.uk>
2023-10-12 19:25:54 +0100

    Fix forms

--- things/forms.py
+++ things/forms.py
@@ -3,10 +3,11 @@
 # Create your forms here.
 
 from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ThingForm(forms.Form):
     """Form for adding things to the database."""
-    name = forms.CharField(max_length=35, required=True)
-    description = forms.Textarea(max_length=120, required=False)
-    quantity = forms.NumberInput(min_value=0, max_value=50, required=True)
+    name = forms.CharField(label="name", max_length=35)
+    description = forms.CharField(label="description", max_length=120, widget=forms.Textarea)
+    quantity = forms.IntegerField(label="quantity", validators=[MinValueValidator(0),MaxValueValidator(50)], widget=forms.NumberInput(attrs={'type':'range', 'step':1, 'value':0, 'onchange':'updateTextInput(this.value);'}))
     
